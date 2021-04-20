823f90dc54daed7e5a7a4f014ac43a8a6c72a15f
Eduardo DOIS <eduardodois@Star-Death.lan>
2021-04-20 08:48:33 -0300

    Add, diadois

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import colors from '../../styles/colors';
 
 import wateringImg from '../assets/watering.png';
 
 export function Welcome() {
   return (
     <View style={styles.container}>
-      <Text>
-        Gerencie suas plantas de forma fácil
+      <Text style={styles.title}>
+        Gerencie {'\n'} 
+        suas plantas {'\n'} 
+        de forma fácil
       </Text>
 
-      <Image source={wateringImg} />
+      <Image source={wateringImg} style={styles.image} />
 
-      <Text>
+      <Text style={styles.subtitle}>
         Não esqueça mais de regrar suas plantas.
         Nós cuidamos de lembrar você sempre que precisar.
       </Text>
 
-      <TouchableOpacity>
-        <Text>
+      <TouchableOpacity style={styles.button} activeOpacity={0.3}>
+        <Text style={styles.buttonText}>
           >
         </Text>
       </TouchableOpacity>
@@ -31,5 +34,37 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: colors.heading,
+    marginTop: 38,
+  },
+  subtitle: {
+    textAlign: 'center',
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading,
+  },
+  button: {
+    backgroundColor: colors.heading,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    paddingHorizontal: 10
+  },
+  image: {
+    width: 292,
+    height: 284
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24
   }
+
+
 })
